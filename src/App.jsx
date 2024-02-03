e9d4df0ef1dcf90a25ddb86623b7e7c4ad22c2a0
Alexander Liu <alexander_l_22@hotmail.com>
2024-02-03 14:04:18 -0500

    new welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import './App.css';
-import HomeBanner from './Components/HomeBanner';
-import Background from './Components/Background';
+import { useState } from 'react';
 
 function App() {
+  const [animationComplete, setAnimationComplete] = useState(false);
+
+  const handleAnimationEnd = () => {
+    setAnimationComplete(true);
+  }
+
   return (
-      <div className='parent-container'>
-        <div className='background-container'>
-          <Background></Background>
-        </div>
-        <div className='profile-container'>
-          <HomeBanner></HomeBanner>
+    <div className='parent-container'>
+      <div className='welcome-text-container'>
+        <div className='welcome-text-wrapper'>
+        <p className={`welcome-text ${animationComplete ? 'animation-complete' : ''}`} onAnimationEnd={handleAnimationEnd}>Welcome to my <span className='highlight-text'>personal</span> website!</p>
         </div>
       </div>
+      <div className={`proceed-button-container ${animationComplete ? 'proceed-button-show' : ''}`}>
+            <button className='proceed-button'>Proceed</button>
+      </div>
+    </div>
   );
 }
 
 export default App;
-
-// https://www.wix.com/website-template/view/html/2622?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fportfolio-cv%2Fpersonal&tpClick=view_button&esi=bcd5be76-0905-430c-a238-325aed3b8eb4
