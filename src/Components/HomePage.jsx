abdc4284428494ebd74499d07768328ed2b5ab93
Alexander Liu <alexander_l_22@hotmail.com>
2024-02-08 17:27:43 -0500

    Updated home page

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import self from "../Assets/profile-headshot.png"
 
 import "../Styles/HomePage.css"
 
 function HomePage() {
     return (
         <div className="home-page-container">
-            <div className="home-nav-bar"></div>
-            <h1>Hi, I'm <span className="highlight-text">Alexander Liu</span></h1>
+            <div className="home-nav-bar">
+                <button className="home-nav-bar-item">Home</button>
+                <button className="home-nav-bar-item">Resume</button>
+                <button className="home-nav-bar-item">Projects</button>
+                <button className="home-nav-bar-item">Contact</button>
+            </div>
+            <div className="home-page">
+                <div className="home-page-intro">
+                    <h3>Welcome to my world!</h3>
+                    <h1>Hi, I'm <span className="highlight-text">Alexander Liu</span><br/>a Software Engineering Student</h1>
+                </div>
+                <img src={self} className="portrait-picture"/>
+            </div>
         </div>
     );
 }
